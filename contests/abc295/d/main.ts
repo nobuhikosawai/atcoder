b20a0a9625aed4f13f9fd23a035e39c1e84c1299
nobuhikosawai <nobuhiko.sawai@gmail.com>
2023-03-26 05:33:04 +0900

    review: abc295 d-2

--- contests/abc295/d/main.ts
+++ contests/abc295/d/main.ts
@@ -2,21 +2,17 @@ import * as fs from "fs";
 
 const inputs = fs.readFileSync("/dev/stdin", "utf-8").trim();
 
-const x: number[][] = Array.from({ length: inputs.length + 1 }).map(() =>
-  Array.from<number>({ length: 10 }).fill(0)
-);
+const x: number[] = Array.from<number>({ length: inputs.length + 1 }).fill(0);
 
 inputs.split("").forEach((c, i) => {
   const num = Number(c);
-  x[i + 1] = [...x[i]];
-  x[i + 1][num] ^= 1;
+  x[i + 1] = x[i] ^ (1 << num);
 });
 
-const m = new Map<string, number>();
+const m = new Map<number, number>();
 
 x.forEach((row) => {
-  const key = row.join("");
-  m.set(key, (m.get(key) || 0) + 1);
+  m.set(row, (m.get(row) || 0) + 1);
 });
 
 let ans = 0;
